05907aae75ca9efc98b701f705bf2bb0bc04c35e
courage <just2courageous@gmail.com>
2025-10-19 16:07:57 -0400

    restore P2 proofs + scripts/policies

---
+++ scripts/set-env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+export AWS_PROFILE="gg"
+export AWS_REGION="us-east-2"
+export CLUSTER="green-guard-gg-eks"
+export ACCOUNT_ID="399717050894"
+export SUBDOMAIN_HOSTED_ZONE="app.courageerhabor.com"
+export APP_HOST="app.courageerhabor.com"
+echo "Env set for gg-ingress-tls."
